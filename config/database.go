a0a9e0e7783b96789fe562f9860b66486837a173
jjambo <joaquimjambo12@gmail.com>
2025-05-02 05:50:39 +0100

    feat configuracao da base dados

--- config/database.go
+++ config/database.go
@@ -8,10 +8,11 @@ import (
 var Db *gorm.DB
 
 func Connect() {
-	dsn := "root:root_password@tcp(localhost:3306)/todolist?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(localhost:3307)/todolist?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 	Db = db
+
 }
